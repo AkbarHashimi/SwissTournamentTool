38e61da89eda1122c3984f7e9aadd247f8be2269
Akbar Hashimi <snakelegendary@gmail.com>
2025-04-08 03:03:39 -0700

    made change to format.h

--- src/format.h
+++ src/format.h
@@ -39,6 +39,7 @@ class Format
 	void removePoints(string playerName);
 	void addPlayer(string playerName);
 	void removePlayer(string playerName);
+	void matchResult(string winnerName); //assumes partner is valid
 
 
 
