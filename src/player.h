7e9f75155ad68fae34c01e80a5c5fe9cd0e3e71d
Akbar Hashimi <snakelegendary@gmail.com>
2025-04-08 01:36:35 -0700

    player.h updated

--- src/player.h
+++ src/player.h
@@ -8,7 +8,65 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
+#include <string>
+using std::string;
 
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Player
+{
+	private:
+	bool Paired;
+	string Pname;
+	int points;
+	Web connectionList;
+
+	public:
+	Player(string name);
+	void addPoint();
+	void removePoint();
+	bool findPair(Player* & foundPlayer);
+	void pair();
+	void unPair();
+	void addConnection(Player* addPlayer);
+	void removeConnection(Player* removePlayer);
+
+
+
+};
+
+//Desc: Node struct for linked lists of players
+struct PlayerNode
+{
+	Player* playerPtr;
+	PlayerNode* next;
+};
+
+//Desc: circular linked list of PlayerNodes representing Players in a format
+struct PlayerLL
+{
+	PlayerNode* head;
+	PlayerNode* tail;
+};
+
+//Desc: A class that contains a linked list of PlayerNode’s and the current matchup.
+class Web
+{
+	private:
+	PlayerLL connections;
+	PlayerNode* currentMatchup;
+
+	public:
+	Web();
+	void addConnection(Player* addPlayer);
+	void removeConnection(Player* removePlayer);
+	~Web();
+
+
+};
 
 
 
