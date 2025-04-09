5ab14259bf30f88245d42f68156855aac3f67ce4
Akbar Hashimi <snakelegendary@gmail.com>
2025-04-09 03:28:57 -0700

    format constructor and destructor set up

--- src/format.cpp
+++ src/format.cpp
@@ -9,3 +9,49 @@ Format::Format(string name)
 	numPlayers = 0;
 	priorityPlayer = nullptr;
 }
+
+Format::~Format()
+{
+	/*
+	playerList.head = nullptr;
+	playerList.tail = nullptr;
+	*/
+
+	//check if empty
+
+	if (playerList.head == nullptr)
+	{
+		cout << endl;
+		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+		cout << endl;
+
+		cout << "PlayerList is empty, nothing to delete" << endl;
+
+		cout << endl;
+		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+		cout << endl;
+
+
+		return;
+	}
+
+	//to delete a circular linked list, first make the tail->next point to nullptr
+
+	PlayerNode* current = nullptr;
+	PlayerNode* next = nullptr;
+
+	playerList.tail->next = nullptr; // makes the linked list non-circular
+
+	current = playerList.head;
+
+	while (current != nullptr)
+	{
+		next = current->next;
+		delete current;
+		current = next;
+
+	}
+
+}
+
+
