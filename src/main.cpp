3b8beefd9fbbfd89ee788efbed66bc1c31174ac8
Akbar Hashimi <snakelegendary@gmail.com>
2025-04-09 02:27:38 -0700

    manual removal of all connections works

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,10 @@ int main()
 	testPlayer.addConnection(&testPlayer3);
 	testPlayer.addConnection(&testPlayer4);
 
+	testPlayer.removeConnection(&testPlayer2); // case - remove last item in list
+	testPlayer.removeConnection(&testPlayer3); // case - remove last item in list
 	testPlayer.removeConnection(&testPlayer4); // case - remove last item in list
+
 
 	testPlayer2.addConnection(&testPlayer);
 	testPlayer2.addConnection(&testPlayer3);
