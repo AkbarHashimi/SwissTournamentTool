b5a02a0f4bd30d1df5cc691b3d58d2a16f122554
Akbar Hashimi <snakelegendary@gmail.com>
2025-04-09 02:18:56 -0700

    case for unfindable player tested and working for - removeConnection()

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main()
 	string name1 = "Akbar";
 	string name2 = "NotAkbar";
 	string name3 = "serious";
+	string name4 = "not in list";
 
 	string getName = "";
 	bool getBool;
@@ -15,14 +16,19 @@ int main()
 	Player testPlayer(name1);
 	Player testPlayer2(name2);
 	Player testPlayer3(name3);
+	Player testPlayer4(name4);
 
 	testPlayer.pair();
 	getName = testPlayer.getName();
 	getBool = testPlayer.getPair();
 
 
+
 	testPlayer.addConnection(&testPlayer2);
 	testPlayer.addConnection(&testPlayer3);
+
+	testPlayer.removeConnection(nullptr);//test for unfindable player
+	testPlayer.removeConnection(&testPlayer4);//test for unfindable player
 
 	testPlayer2.addConnection(&testPlayer);
 	testPlayer2.addConnection(&testPlayer3);
